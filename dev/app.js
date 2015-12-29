dbc4a5f760fd988a9aebafc485b2a01734815e63
Ricardo <landinezrichard@gmail.com>
2015-12-29 17:12:44 -0500

    Añadidas icon-fonts y lazy load de CSS

--- dev/app.js
+++ dev/app.js
@@ -1,11 +1,14 @@
 (function () {
 	const showMenu = require('./components/MainMenu');
 	const changeHeader = require('./components/Header');
+	const loadCSS = require('./lib/loadCSS.js')
 
 	document.addEventListener('DOMContentLoaded', onDOMload);
 
 	function onDOMload() {
+		loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css');
 		showMenu.init();
 		changeHeader.init();
+
 	}
 }())
